5f1af6fa2a0f516ae57102cb50da982d0e87e3b9
bongiBong-create <mairanss543@inbox.ru>
2025-02-12 20:26:07 +0300

    +base

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,13 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Character } from "../pages/character";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/:id",
+      element: <Character />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/:id",
-    element: <Character />,
-  },
-]);
+    basename: "/RickAndMortyTest/",
+  }
+);
